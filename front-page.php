59aac349091d65b8eec6afc37dbfcf3863ba8365
contentJones <pd@pauldavidson.info>
2015-05-30 07:18:30 -0600

    Set up wp_query for posts with a category_name of "work"

--- front-page.php
+++ front-page.php
@@ -22,27 +22,45 @@ get_header(); ?>
 	<h2 class="subhead">recent work</h2>		
 
 		<!-- begin loop -->
-		<?php while ( have_posts() ) : the_post(); ?>
+		<?php
+			
+			// The Query
+			$args = array(
+				'category_name' => 'work',
+				'orderby' => 'date',
+				'order' => 'DESC'
+			);
+			$blog_query = new WP_Query( $args );
+
+			if ( $blog_query->have_posts() ) :
+			
+			while ( $blog_query->have_posts() ) : $blog_query->the_post(); ?>
+
 		
 		<!-- item 1 -->
 		<div class="featured-item">
 			<div class="wrap">
 				<div class="featured-excerpt">
-					<h3><a href="<?php echo get_post_meta( get_the_ID(), 'work-link', true ); ?>"><?php echo get_the_title(); ?></a></h3>
+					<h3><a href="<?php echo get_post_meta( get_the_ID(), 'work-link', true ); ?>" target="_blank"><?php echo get_the_title(); ?></a></h3>
 					<p><?php the_content(); ?></p>
 <!-- 					<a class="button" href="#">Visit the site</a> -->
 				</div>
 				<div class="featured-image">
-					<!-- make these lightbox images -->
-					<?php if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-	the_post_thumbnail();
-} ?>
+					<!-- get full-sized featured image and link to the media file -->
+					<?php if ( has_post_thumbnail() ) {
+								$full_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
+								echo '<a href="' . $full_image_url[0] . '" title="' . the_title_attribute( 'echo=0' ) . '">';
+								the_post_thumbnail( 'full' );
+								echo '</a>';
+							} ?>
 				</div>
 			</div>
 		</div>
 				
 		<!-- end of loop -->
-		<?php endwhile; // end of the loop. ?>
+		<?php endwhile; 
+			  endif;
+			  wp_reset_postdata(); ?>
 		
 	</section>		
 
